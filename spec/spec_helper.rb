1aad2db5d0251cb450f5e0629ae4138e34bb0a92
Christoffer Lervåg <chris.lervag@gmail.com>
2013-07-04 16:17:40 +0200

    Add a CR DICOM image sample

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ suppress_warnings do
   RTKIT::DIR_PLAN_ONLY = 'samples/single_plan/'
   RTKIT::DIR_DOSE_ONLY = 'samples/single_dose/'
   RTKIT::DIR_RTIMAGE_ONLY = 'samples/single_rt_image/'
+  RTKIT::DIR_CR_ONLY = 'samples/single_cr/'
   # A simple 5 slice phantom with 8 contours:
   RTKIT::DIR_SIMPLE_PHANTOM_CONTOURS = 'samples/simple_phantom_contours/'
   # A simple 5 slice phantom with 8 contours, a 3 beam plan, corresponding dose grids and RT Images:
@@ -35,6 +36,7 @@ suppress_warnings do
   RTKIT::FILE_PLAN = RTKIT::DIR_PLAN_ONLY + 'simple_plan.dcm'
   RTKIT::FILE_DOSE = RTKIT::DIR_DOSE_ONLY + 'dose.dcm'
   RTKIT::FILE_RTIMAGE = RTKIT::DIR_RTIMAGE_ONLY + 'rt_image.dcm'
+  RTKIT::FILE_CR = RTKIT::DIR_CR_ONLY + 'cr_image.dcm'
   RTKIT::FILE_BRACHY_PLAN = 'samples/single_brachy_plan/brachy_plan.dcm'
   # Single, 'special' files:
   RTKIT::FILE_EMPTY_DOSE = 'samples/single_empty_dose/' + 'empty_dose.dcm'
